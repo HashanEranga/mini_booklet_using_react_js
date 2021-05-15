3da3b183f0d7fb802deef27b7ab7aa39611707d8
Hashan Eranga <ccmrhe@gmail.com>
2021-05-16 03:15:47 +0530

    created a working initial site

---
+++ mini_book_let/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+function Booklet() {
+  return (
+    <h1>This is the booklet</h1>
+  )
+}
+
+ReactDOM.render(<Booklet/>, document.getElementById('root'));
