e3d69322543020f9f578ae5cadbc7acc3c17b36d
Hashan Eranga <ccmrhe@gmail.com>
2021-05-16 20:04:38 +0530

    method of creating a props using object definition

--- mini_book_let/src/index.js
+++ mini_book_let/src/index.js
@@ -5,8 +5,9 @@ import './index.css'
 
 function Booklet() {
   return (
-    <section className="booklet">
-      <div className="booksection">
+    <section className="book__let">
+      <h1>Book Section</h1>
+      <div className="book__section">
         <Book 
             img = "https://m.media-amazon.com/images/I/51mY5a8y1FL._AC_UY218_.jpg"
             title = "Liberal Education and Its Discontents: The Crisis in the Indian University"
@@ -18,10 +19,22 @@ function Booklet() {
             title = "A text Book of Ayurvedic Physiology ( sharir Kriya Vijnana)"
             author = "Prof. S. B. Kotur and Dr. Shashikala Kotur"
         />
+
+        <Book
+            img = {firstBook.img}
+            title = {firstBook.title}
+            author = {firstBook.author}
+        />
       </div>
         
     </section>
   )
+}
+
+const firstBook = {
+    img : "https://m.media-amazon.com/images/I/51hPqwsUw5L._AC_UY218_.jpg",
+    title : "A text Book of Ayurvedic Physiology ( sharir Kriya Vijnana)",
+    author : "Prof. S. B. Kotur and Dr. Shashikala Kotur"
 }
 
 const Book = (props) => {
