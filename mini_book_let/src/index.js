6c4fa35a2407572ba2b5c7c9dd77a30552e8d1b0
Hashan Eranga <ccmrhe@gmail.com>
2021-05-16 22:22:45 +0530

    passing a list as props

--- mini_book_let/src/index.js
+++ mini_book_let/src/index.js
@@ -1,34 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Book from './Book'
 import './index.css'
 
 function Booklet() {
-  return (
+  return(
     <section className="booklet">
-      <Book 
-        img = {firstBook.img}
-        title = {firstBook.title}
-        author = {firstBook.author}
-      >
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni illo omnis asperiores earum ex ipsa id quidem non doloremque.</p>
-      </Book>
-        
-      <Book
-        img = "https://m.media-amazon.com/images/I/51mY5a8y1FL._AC_UY218_.jpg"
-        title = "Liberal Education and Its Discontents: The Crisis in the Indian University"
-        author = "Shashikala Srinivasan"
-      />
-
+      {newList}
     </section>
-
   )
 }
 
-const firstBook = {
-  img : "https://m.media-amazon.com/images/I/51mY5a8y1FL._AC_UY218_.jpg",
-  title : "Liberal Education and Its Discontents: The Crisis in the Indian University",
-  author : "Shashikala Srinivasan"
-}
+const List = ['hashan', 'eranga', 'perera'];
+const newList = List.map((items) => {
+  return(
+    <h1>{items}</h1>
+  )
+})
 
 ReactDOM.render(<Booklet/>, document.getElementById('root'));
