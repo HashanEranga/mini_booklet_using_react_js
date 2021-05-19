1d27a6771e9c6fce6748fc05e59e992ec605024d
Hashan Eranga <ccmrhe@gmail.com>
2021-05-19 15:05:49 +0530

    export booklist to another file

--- mini_book_let/src/Booklet.js
+++ mini_book_let/src/Booklet.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Book from './Book'
+import { books } from './BookList'
 
 function Booklet() {
     return(
@@ -17,34 +18,5 @@ function Booklet() {
     )
   }
   
-  const books = [
-    {
-      id: 1,
-      img : "https://images-na.ssl-images-amazon.com/images/I/41+2DiWeWAS._AC_SX184_.jpg",
-      title : "The Premonition: A Pandemic StoryThe Premonition: A Pandemic Story ",
-      author : "Michael Lewis"
-    },
-    
-    {
-      id: 2,
-      img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-      title : "The Good Sister: A NovelThe Good Sister: A Novel",
-      author : "Sally Hepworth"
-    },
-  
-    {
-      id: 3,
-      img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-      title : "The Good Sister: A NovelThe Good Sister: A Novel",
-      author : "Sally Hepworth"
-    },
-  
-    {
-      id: 4,
-      img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-      title : "The Good Sister: A NovelThe Good Sister: A Novel",
-      author : "Sally Hepworth"
-    },
-  ];
 
 export default Booklet
