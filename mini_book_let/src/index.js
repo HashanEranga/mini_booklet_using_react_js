05f7ed1c5f15499a48b42ca4d95a0f448607605c
Hashan Eranga <ccmrhe@gmail.com>
2021-05-19 15:03:01 +0530

    manage all the react files with components

--- mini_book_let/src/index.js
+++ mini_book_let/src/index.js
@@ -1,52 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Book from './Book';
+import Booklet from './Booklet';
 import './index.css'
 
-function Booklet() {
+function Index() {
   return(
-    <section className="booklet">
-      
-      {books.map((book)=>{
-        return (
-          <Book 
-            key = {book.id}
-            {...book}
-          />
-        )
-      })}
-    </section>
+    <>
+    <Booklet/>
+    <Booklet/>
+    </>
   )
 }
 
-const books = [
-  {
-    id: 1,
-    img : "https://images-na.ssl-images-amazon.com/images/I/41+2DiWeWAS._AC_SX184_.jpg",
-    title : "The Premonition: A Pandemic StoryThe Premonition: A Pandemic Story ",
-    author : "Michael Lewis"
-  },
-  
-  {
-    id: 2,
-    img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-    title : "The Good Sister: A NovelThe Good Sister: A Novel",
-    author : "Sally Hepworth"
-  },
-
-  {
-    id: 3,
-    img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-    title : "The Good Sister: A NovelThe Good Sister: A Novel",
-    author : "Sally Hepworth"
-  },
-
-  {
-    id: 4,
-    img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
-    title : "The Good Sister: A NovelThe Good Sister: A Novel",
-    author : "Sally Hepworth"
-  },
-];
-
-ReactDOM.render(<Booklet/>, document.getElementById('root'));
+ReactDOM.render(<Index/>, document.getElementById('root'));
