08eb62a4bfbf49cbf98b115eb76b49f56508e76c
Hashan Eranga <ccmrhe@gmail.com>
2021-05-19 13:43:38 +0530

    key operator for props remove the error

--- mini_book_let/src/index.js
+++ mini_book_let/src/index.js
@@ -9,7 +9,7 @@ function Booklet() {
       
       {books.map((book)=>{
         return (
-          <Book 
+          <Book key = {book.id}
             book={book}
           />
         )
@@ -20,24 +20,28 @@ function Booklet() {
 
 const books = [
   {
+    id: 1,
     img : "https://images-na.ssl-images-amazon.com/images/I/41+2DiWeWAS._AC_SX184_.jpg",
     title : "The Premonition: A Pandemic StoryThe Premonition: A Pandemic Story ",
     author : "Michael Lewis"
   },
   
   {
+    id: 2,
     img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
     title : "The Good Sister: A NovelThe Good Sister: A Novel",
     author : "Sally Hepworth"
   },
 
   {
+    id: 3,
     img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
     title : "The Good Sister: A NovelThe Good Sister: A Novel",
     author : "Sally Hepworth"
   },
 
   {
+    id: 4,
     img : "https://images-na.ssl-images-amazon.com/images/I/51zMKWZCRfS._AC_SX184_.jpg",
     title : "The Good Sister: A NovelThe Good Sister: A Novel",
     author : "Sally Hepworth"
