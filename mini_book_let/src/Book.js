b1d69f8c871c8012c7e13cbe6ca4d98f0a4b742f
Hashan Eranga <ccmrhe@gmail.com>
2021-05-19 14:48:35 +0530

    some basics of event handling is created

--- mini_book_let/src/Book.js
+++ mini_book_let/src/Book.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
-function Book(props) {
-    const {img, title, author } = props;
+function Book({img, title, author}) {
+    const clickHandler = () =>{
+        alert('hello world');
+    }
+
+    const complexExample = (author) => {
+        console.log(author)
+    }
+
     return (
-        <div className="book">
+        <div className="book" onMouseOver={()=>console.log(title)}>
             
             <img src={img} alt="" />
-            <h1>{title}</h1>
+            <h1 onClick={() => {
+                console.log('hello')
+            }}>{title}</h1>
             <p>{author}</p>
-          
+            <button type="botton" onClick={clickHandler}>
+                reference button
+            </button>
+            <button type='button' onClick={() => complexExample(author)}>
+                more complex button
+            </button>
         </div>
     )
 }
